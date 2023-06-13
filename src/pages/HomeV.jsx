debffb0c68e2e75c6ef953bfee51c507e0762ada
TifanyAlmeida <ticaanahi@outlook.com>
2023-06-13 17:05:28 -0300

    17h05

--- src/pages/HomeV.jsx
+++ src/pages/HomeV.jsx
@@ -1,33 +1,59 @@
 import styles from '../styles/Login.js';
 import React, { useState } from 'react';
 import { Button, Image, View, TextInput, Text} from 'react-native';
+// import * as Permissions from 'expo-permissions';
 import * as ImagePicker from 'expo-image-picker';
 import { mutate } from 'swr';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeE from './HomeE.jsx'
+import HomeC from './HomeC.jsx'
 
-export default function HomeC({ navigation }) {
 
+export default function HomeV() {
+  const Tab = createBottomTabNavigator();
   const [nome, setNome] = useState('')
   const [preco, setPreco] = useState('')
   const [qtd, setQtd] = useState('')
-
   const [image, setImage] = useState(null);
+  const [base64, setBase64] = useState('')
+
   const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      base64: true,
       allowsEditing: true,
       aspect: [4, 3],
       quality: 1,
     });
     if (!result.canceled) {
       setImage(result.assets[0].uri);
+      setBase64(result.assets[0].base64)
+      console.log(result)
     }
   };
-
+  
   const cadastrarProd = async () =>{
+
+    const filename = image.substring(image.lastIndexOf('/') + 1, image.length)
+    const extensao = filename.substring(filename.lastIndexOf('.') + 1, filename.lenght) //testar com + 1
+    const formData = new FormData() 
+    console.log(filename)
+    console.log(extensao) // testar *
+    formData.append('file', JSON.parse(JSON.stringify({
+      name: nome,
+      uri: image,
+      type: extensao,
+    })))
+
     await fetch('http://localhost:3000/api/produto', {
+      // formData,
       method: 'POST',
       body: JSON.stringify({nome, preco, qtd}),
-      headers: {'Content-Type': 'application/json'}
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Type': 'multipart/form-data',
+      }
     })
     mutate('http://localhost:3000/api/produto')
     setNome('')
@@ -37,6 +63,7 @@ export default function HomeC({ navigation }) {
   }
 
   return (
+    <>
     <View style={styles.container}>
       <Text style={styles.titulo}>Cadastro de Produtos</Text>
       {image && <Image source={{ uri: image }} style={styles.img} />}
@@ -48,5 +75,12 @@ export default function HomeC({ navigation }) {
         <Button title="CADASTRAR" style={styles.button} onPress={cadastrarProd}></Button>
       </View>
     </View>
+      <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeC} />
+        <Tab.Screen name="Settings" component={HomeE} />
+      </Tab.Navigator>
+    </NavigationContainer>
+    </>
   );
 }
